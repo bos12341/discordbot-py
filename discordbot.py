07981839a62c00bc2e966cfae434f0beab12f806
bos12341 <122173560+bos12341@users.noreply.github.com>
2023-05-15 03:20:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,20 +19,6 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('-버튼'):
-        # remove the prefix and get the url
-        url = message.content[4:].strip()
-
-        # create the button
-        button = discord.Button(label='클릭', url=url)
-
-        # generate an embed with the button
-        embed = discord.Embed(title='클릭하세요', description='')
-        embed.add_field(name='\u200b', value=f'{button}')
-
-        # send the message
-        await message.channel.send(embed=embed)
-    
     if message.content.startswith('-인증'):
         if message.channel.id != 1099558481519980658: # 지정된 채널의 ID를 입력해주세요
             await message.channel.send("인증은 지정된 채널에서만 가능합니다.")
